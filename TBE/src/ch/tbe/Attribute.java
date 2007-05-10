866b042ad3f5f31dd6e265950f21e2798d4548c7
newsletter <newsletter>
2007-05-10 16:53:22 +0000

    *** empty log message ***

--- TBE/src/ch/tbe/Attribute.java
+++ TBE/src/ch/tbe/Attribute.java
@@ -3,20 +3,19 @@ package ch.tbe;
 public class Attribute {
  
 	private String text;
-	 
 	private String title;
 	 
 	public void Attribute(String text, String title) {
+		this.text = text;
+		this.title = title;
 	}
 	 
 	public void setText(String text) {
+		this.text = text;
 	}
 	 
 	public void setTitle(String title) {
+		this.title = title;
 	}
-	 
-	public void Message2() {
-	}
-	 
 }
  
