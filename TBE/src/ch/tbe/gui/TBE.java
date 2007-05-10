4bf5cd2cd6442c1d4febf1ce0e1689e9e9c0a79f
newsletter <newsletter>
2007-05-10 11:36:32 +0000

    *** empty log message ***

--- TBE/src/ch/tbe/gui/TBE.java
+++ TBE/src/ch/tbe/gui/TBE.java
@@ -5,76 +5,104 @@ import ch.tbe.Sport;
 import ch.tbe.Field;
 import java.util.List;
 
-public class TBE {
- 
+import javax.swing.JFrame;
+
+
+
+public class TBE
+{
+
 	private ArrayList sports;
-	 
+
 	private String UserName;
-	 
+
 	private String UserPrename;
-	 
+
 	private String UserEmail;
-	 
-	public ArrayList getSports() {
+	private final int HEIGHT = 300;
+	private final int WIDTH = 600;
+
+	public TBE()
+	{
+		this.init();
+	}
+	private void init(){
+		JFrame f = new JFrame("TBE");
+		f.setSize(this.WIDTH, this.HEIGHT);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		
+	}
+
+	public ArrayList getSports()
+	{
 		return null;
 	}
-	 
-	public void createBoard(Sport sport) {
+
+	public void createBoard(Sport sport)
+	{
 	}
-	 
-	public void saveAs() {
+
+	public void saveAs()
+	{
 	}
-	 
-	public void save() {
+
+	public void save()
+	{
 	}
-	 
-	public void load(String path) {
+
+	public void load(String path)
+	{
 	}
-	 
-	public void changeField(Field field) {
+
+	public void changeField(Field field)
+	{
 	}
-	 
-	public void clear() {
+
+	public void clear()
+	{
 	}
-	 
-	public void load() {
+
+	public void load()
+	{
 	}
-	 
-	public void share() {
+
+	public void share()
+	{
 	}
-	 
-	public void undo() {
+
+	public void undo()
+	{
 	}
-	 
-	public void redo() {
+
+	public void redo()
+	{
 	}
-	 
-	public void setUser(String name, String prename, String email) {
+
+	public void openSettings()
+	{
 	}
-	 
-	public void TBE() {
+
+	public void addFTPServer(String name, String host, int port,
+			String username, String password)
+	{
 	}
-	 
-	public void openSettings() {
+
+	public void editFTPServer(String name, String host, int port,
+			String username, String password)
+	{
 	}
-	 
-	public void showAllSports() {
+
+	public void removeFTPServer(String name)
+	{
 	}
-	 
-	public void installSports(List sports) {
+
+	public void setUser(String prename, String lastname, String email,
+			String language)
+	{
 	}
-	 
-	public void addFTPServer(String name, String host, int port, String username, String password) {
+	public static void main(String[] args) {
+		new TBE();
 	}
-	 
-	public void editFTPServer(String name, String host, int port, String username, String password) {
-	}
-	 
-	public void removeFTPServer(String name) {
-	}
-	 
-	public void setUser(String prename, String lastname, String email, String language) {
-	}
-	 
+
 }
- 
