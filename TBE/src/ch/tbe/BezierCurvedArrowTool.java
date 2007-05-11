82bfd476980fbc11f1269612f140535f03be8dce
newsletter <newsletter>
2007-05-11 12:05:05 +0000

    *** empty log message ***

--- TBE/src/ch/tbe/BezierCurvedArrowTool.java
+++ TBE/src/ch/tbe/BezierCurvedArrowTool.java
@@ -1,6 +1,21 @@
 package ch.tbe;
 
-public class BezierCurvedArrowTool {
+import ch.tbe.framework.ItemComponent;
+import ch.tbe.framework.Tool;
+
+public class BezierCurvedArrowTool implements Tool{
+
+	public void Tool()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ItemComponent create(int x, int y)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
  
 }
  
