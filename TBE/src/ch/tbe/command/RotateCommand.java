ec5bb8ac0cd38ed8a747ecdb6257dbe59d6d546d
newsletter <newsletter>
2007-06-14 07:29:32 +0000

    *** empty log message ***

---
+++ TBE/src/ch/tbe/command/RotateCommand.java
@@ -0,0 +1,25 @@
+package ch.tbe.command;
+
+import ch.tbe.framework.Command;
+import ch.tbe.framework.ItemComponent;
+
+public class RotateCommand  extends Command
+{
+
+	public RotateCommand(ItemComponent[] items)
+	{
+		super(items);
+		
+	}
+
+	public void redo()
+	{
+		//TODO 
+	}
+
+	public void undo()
+	{
+		//TODO
+		
+	}
+}
