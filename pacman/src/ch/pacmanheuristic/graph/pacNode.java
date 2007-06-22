438cc1f11702d693b52765d02fff4ee807b68b1c
newsletter <newsletter>
2007-06-22 14:13:26 +0000

    *** empty log message ***

---
+++ pacman/src/ch/pacmanheuristic/graph/pacNode.java
@@ -0,0 +1,39 @@
+package ch.pacmanheuristic.graph;
+
+import jdsl.core.api.InvalidAttributeException;
+import jdsl.core.api.ObjectIterator;
+import jdsl.graph.api.*;
+
+public class pacNode implements Vertex {
+
+	public Object element() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ObjectIterator attributes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object destroy(Object arg0) throws InvalidAttributeException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object get(Object arg0) throws InvalidAttributeException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean has(Object arg0) throws InvalidAttributeException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void set(Object arg0, Object arg1) throws InvalidAttributeException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
